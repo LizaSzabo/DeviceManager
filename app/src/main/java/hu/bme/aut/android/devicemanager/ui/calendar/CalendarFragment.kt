34d59d22cec4a5763364c539b5e7beb86bc438da
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-11-19 00:12:43 +0100

    add new design for calendar

--- app/src/main/java/hu/bme/aut/android/devicemanager/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/hu/bme/aut/android/devicemanager/ui/calendar/CalendarFragment.kt
@@ -1,13 +1,15 @@
 package hu.bme.aut.android.devicemanager.ui.calendar
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.view.size
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
-import com.prolificinteractive.materialcalendarview.CalendarDay
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.aut.android.devicemanager.R
 import hu.bme.aut.android.devicemanager.databinding.FragmentCalendarBinding
@@ -32,20 +34,31 @@ class CalendarFragment : RainbowCakeFragment<CalendarViewState, CalendarViewMode
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // binding.calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_RANGE
 
-        binding.calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_RANGE
+        setupCalendar()
 
-     /*   val calendar: Calendar = Calendar.getInstance()
-        calendar.set(2017, 11, 14)
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.set(2022, 11, 14)
 
         val calendar1: Calendar = Calendar.getInstance()
-        calendar1.set(2017, 11, 16)
+        calendar1.set(2022, 11, 16)
 
-        binding.calendarView.setDateSelected(CalendarDay.today(), true)*/
+        binding.calendarView.setDisabledDays(listOf(calendar, calendar1))
+        /* binding.calendarView.setDateSelected(CalendarDay.today(), true)*/
+
 
     }
 
     override fun render(viewState: CalendarViewState) {
         //TODO("Not yet implemented")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setupCalendar() {
+        binding.calendarView.setHeaderColor(R.color.main_variant_orange)
+        binding.calendarView.setHeaderLabelColor(R.color.white)
+        binding.calendarView.setAllowClickWhenDisabled(false)
+
     }
 }
