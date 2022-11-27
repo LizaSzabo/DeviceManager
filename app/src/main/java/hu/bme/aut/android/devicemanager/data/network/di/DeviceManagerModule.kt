905eddb57a26ac455a3a03aaa60996ff7c64e2bc
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-11-27 22:59:38 +0100

    add https clint

--- app/src/main/java/hu/bme/aut/android/devicemanager/data/network/di/DeviceManagerModule.kt
+++ app/src/main/java/hu/bme/aut/android/devicemanager/data/network/di/DeviceManagerModule.kt
@@ -9,7 +9,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 
 @Module
@@ -18,16 +25,64 @@ class DeviceManagerModule {
 
     companion object {
         const val BaseUrl =
-            "http://Devicerenter-env.eba-xkymi3qb.eu-central-1.elasticbeanstalk.com/api/"
+            "https://Devicerenter-env.eba-xkymi3qb.eu-central-1.elasticbeanstalk.com/api/"
     }
 
-    var interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    /*For logging network traffic*/
+    var interceptor = run {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    /*For http client*/
     private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
+    /*For https client*/
+    private fun getUnsafeOkHttpClient(): OkHttpClient.Builder? {
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts: Array<TrustManager> = arrayOf(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> {
+                        return arrayOf()
+                    }
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext: SSLContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier { _, _ -> true }
+            builder
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BaseUrl)
-        .client(client)
+        .client(getUnsafeOkHttpClient()!!.build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
